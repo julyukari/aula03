957190d613c3ab5f6f5828f11d8f68abdb1dad6a
july yukari <julyukari.cdd@gmail.com>
2025-04-01 08:54:14 -0300

    excercicio3

---
+++ aula03.py
@@ -0,0 +1,7 @@
+nome= input("Digite seu nome:")
+idade= int(input("Digite sua idade:"))
+salario= float(input("informe seu salario:"))
+print (f"Olá {nome}:, você tem {idade} anos, seu salario é {salario}")
+
+
+
