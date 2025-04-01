59b85ff04c113cc9ba197191d8b3e8b4b14e31ae
july yukari <julyukari.cdd@gmail.com>
2025-04-01 09:20:33 -0300

    alteração do exercicio03

--- aula03.py
+++ aula03.py
@@ -1,7 +1,13 @@
-nome= input("Digite seu nome:")
-idade= int(input("Digite sua idade:"))
-salario= float(input("informe seu salario:"))
-print (f"Olá {nome}:, você tem {idade} anos, seu salario é {salario}")
+nome= input("Digite seu nome: ")
+idade= int(input("Digite sua idade: "))
+salario= float(input("informe seu salario: "))
+aumento= float(input("digite a porcetagem do seu aumento: "))
+aumentoreal= salario * aumento/100
+novosalario= salario + aumentoreal
+print (f"Olá {nome}, você tem {idade} anos \n"
+       f" e seu salario é {salario: .2f},você recebeu {aumentoreal: .2f} de aumento, seu salario agora é{novosalario: .2f}")
+
+
 
 
 
