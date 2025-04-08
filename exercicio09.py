2ec52f173a3b92c96104e6f0abee06a893d0f7bc
july yukari <julyukari.cdd@gmail.com>
2025-04-08 09:31:51 -0300

    exercicio09

---
+++ exercicio09.py
@@ -0,0 +1,29 @@
+mes= int(input("digite um numero: "))
+if mes== 1:
+    print("janeiro")
+elif mes == 2:
+    print("fevereiro")
+if mes == 3:
+     print("marco")
+elif mes == 4:
+    print("abril")
+if mes == 5:
+    print("maio")
+elif mes == 6:
+    print("junho")
+if mes == 7:
+    print("julho")
+elif mes == 8:
+    print("agosto")
+if mes == 9:
+    print("setembro")
+elif mes == 10:
+    print("outubro")
+if mes == 11:
+    print("novembro")
+elif mes == 12:
+    print("dezembro")
+if mes <=0:
+    print("mes invalido")
+elif mes >12:
+    print("mes invalido")
